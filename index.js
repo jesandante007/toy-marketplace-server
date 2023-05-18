8626ec5b6567f56ce6e4b83a2b145b741b24fb41
jesandante007 <jesandante007@gmail.com>
2023-05-18 23:06:00 +0600

    category wise filtered

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(express.json())
-
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jtzepo9.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -17,16 +16,27 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const toyCollection = client.db("toyDB").collection("toys");
+
+    app.get("/toys/:category", async (req, res) => {
+      const category = req.params.category;
+      const filter = { category: category };
+      const result = await toyCollection.find(filter).toArray();
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -34,11 +44,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-app.get('/', (req, res) => {
-    res.send('toy-marketplace server is running')
-})
+app.get("/", (req, res) => {
+  res.send("toy-marketplace server is running");
+});
 
 app.listen(port, () => {
-    console.log(`toy-marketplace server is running on port ${port}`);
-})
+  console.log(`toy-marketplace server is running on port ${port}`);
+});
